356b0ee243b2e33cb64b9ec4550a1706f1bbd6b8
homeless2010 <15594821882@163.com>
2019-07-29 18:46:17 +0800

    init project

---
+++ src/main/java/xin/cuichaojiang/tubercle/TubercleApplication.java
@@ -0,0 +1,13 @@
+package xin.cuichaojiang.tubercle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubercleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TubercleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xin/cuichaojiang/tubercle/TubercleApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.cuichaojiang.tubercle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubercleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
